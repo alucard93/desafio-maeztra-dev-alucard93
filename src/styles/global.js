a5e707df7bc538d40c06785052753cba44552bbb
Marcus Vinicius Nascimento Ferreira <103126863+alucard93@users.noreply.github.com>
2024-01-12 01:07:12 -0300

    feat(deploy): configuração do deploy

---
+++ src/styles/global.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :focus {
+        outline: 0;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    body {
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-weight: 400;
+        font-size: 1rem;
+    }
+
+    .newsletter-btn,
+    .container-user-mid-header {
+        cursor: pointer;
+    }
+`
